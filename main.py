4d1d66615bbeac68c1688ad37174b32ec8a115e9
SergeiB-web <sbury7596@gmail.com>
2022-09-13 19:47:08 +0300

    EXCEPTIONS

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-type
+#a = input("чbсло")
+#b = input("число")
+#try:
+#    print(int(a)+int(b))
+    #except (ValueError,TypeError) :
+#    print("Нельзя складывать строку с числом")
+#    print("Нельзя складывать строку со строкой")
+
+from datetime import datetime
+human = datetime(1996, 9, 25)
+print(human)
+now = datetime.now()
+print(now.date())
+print(now.year - human.year)
+
